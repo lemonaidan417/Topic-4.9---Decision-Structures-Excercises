bd36d4f32e9a1a2f05f4cbb5ee94899a187cee60
339501421 <339501421@2260I-L29-001>
2024-02-22 12:44:59 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Topic_4._9___Decision_Structures_Excercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int personOne, personTwo, personThree;
+
+
+            personOne = Convert.ToInt32(Console.ReadLine());
+
+            personTwo = Convert.ToInt32(Console.ReadLine());
+
+            personThree = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
